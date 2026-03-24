de540311956a86ef5b9f5534f8b036b974b6b0cc
Ari Perkkiö <ari.perkkio@gmail.com>
2026-03-24 15:47:49 +0200

    feat: expose `takeSnapshot()` as public API

--- packages/vitest/src/browser/setupFile.ts
+++ packages/vitest/src/browser/setupFile.ts
@@ -7,11 +7,21 @@ import type {} from '../node/commands';
 // Destructuring the context is important, so that possible user-provided fixtures work too
 beforeEach<InternalTestContext>(async ({ task }) => {
   task.meta.__chromatic_isRegistered = true;
+  task.meta.__chromatic_isTakeSnapshotCalled = false;
 
   await commands.__chromatic_interceptFetch(task.id);
 
   return async function afterEach() {
-    await takeSnapshot();
+    const { __chromatic_pendingTakeSnapshots: pendingTakeSnapshots } = task.meta;
+
+    if (pendingTakeSnapshots?.length) {
+      throw new AggregateError(
+        pendingTakeSnapshots.map((call) => call.error),
+        `${pendingTakeSnapshots.length} unawaited takeSnapshot() call(s)`
+      );
+    }
+
+    await takeSnapshot(undefined, { ignoreUnawaited: true });
 
     await commands.__chromatic_writeTestResult(task.id);
 
@@ -22,6 +32,8 @@ beforeEach<InternalTestContext>(async ({ task }) => {
    * Clean internal task meta so that it doesn't show up on Vitest's reporters
    */
   function cleanup() {
+    task.meta.__chromatic_isTakeSnapshotCalled = undefined;
     task.meta.__chromatic_isRegistered = undefined;
+    task.meta.__chromatic_pendingTakeSnapshots = undefined;
   }
 });
