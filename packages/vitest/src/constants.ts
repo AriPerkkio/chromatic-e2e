41e9c5154b482d50f927391b7b56d937d0c03421
Ari Perkkiö <ari.perkkio@gmail.com>
2026-03-24 10:59:48 +0200

    feat: add `@chromatic-com/vitest`, minimal setup

---
+++ packages/vitest/src/constants.ts
@@ -0,0 +1,2 @@
+// TODO: Use directory that's decided in https://github.com/vitest-dev/vitest/issues/9952
+export const DEFAULT_OUTPUT_DIR = process.cwd();
---
+++ packages/vitest/tsup.config.ts
@@ -0,0 +1,53 @@
+import { defineConfig } from 'tsup';
+
+export default defineConfig([
+  {
+    name: 'CJS Node bins',
+    entry: {
+      'bin/archive-storybook': 'src/bin/archive-storybook.ts',
+      'bin/build-archive-storybook': 'src/bin/build-archive-storybook.ts',
+    },
+    platform: 'node',
+    format: 'cjs',
+    splitting: false,
+    minify: false,
+  },
+  {
+    name: 'CJS Node',
+    entry: {
+      'storybook-config/main': 'src/storybook-config/main.ts',
+      'storybook-config/manager': 'src/storybook-config/shared/manager.ts',
+    },
+    platform: 'node',
+    format: 'cjs',
+    outExtension: () => ({ js: '.cjs' }),
+    splitting: false,
+    minify: false,
+  },
+  {
+    name: 'ESM Node',
+    entry: {
+      plugin: 'src/node/plugin.ts',
+      setupFile: 'src/browser/setupFile.ts',
+    },
+    platform: 'node',
+    format: 'esm',
+    outExtension: () => ({ js: '.mjs' }),
+    splitting: false,
+    minify: false,
+    dts: { resolve: true },
+  },
+  {
+    name: 'ESM Browser',
+    entry: {
+      index: 'src/index.ts',
+      'storybook-config/preview': 'src/storybook-config/shared/preview.ts',
+    },
+    platform: 'browser',
+    format: 'esm',
+    outExtension: () => ({ js: '.mjs' }),
+    splitting: false,
+    dts: { resolve: true },
+    minify: false,
+  },
+]);
