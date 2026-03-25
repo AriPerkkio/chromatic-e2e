e6520e66290dbf70d1ae83c3d1df5fff2a08f1fa
Ari Perkkiö <ari.perkkio@gmail.com>
2026-03-25 10:57:38 +0200

    feat: add `disableAutoSnapshot: boolean` plugin option and `disableAutoSnapshot()` API

--- packages/vitest/vitest.config.browser.ts
+++ packages/vitest/vitest.config.browser.ts
@@ -31,5 +31,6 @@ declare module 'vitest' {
   export interface ProvidedContext {
     processCwd: string;
     testName?: string;
+    disableAutoSnapshot: 'module' | 'describe' | 'describe-nested' | 'test' | 'test-second';
   }
 }
