5ec5e6ed33aaff09d0ab3d098d8a5803efbace54
Ari Perkkiö <ari.perkkio@gmail.com>
2026-03-25 10:57:38 +0200

    feat: add `disableAutoSnapshot: boolean` plugin option and `disableAutoSnapshot()` API

--- packages/vitest/src/node/plugin.ts
+++ packages/vitest/src/node/plugin.ts
@@ -13,6 +13,7 @@ import { type ResolvedOptions, type Options } from '../types';
 export function chromaticPlugin(userOptions: Options = {}): Vite.Plugin {
   const options: ResolvedOptions = {
     assetDomains: [],
+    disableAutoSnapshot: false,
     outputDirectory: process.cwd(),
     resourceArchiveTimeout: DEFAULT_GLOBAL_RESOURCE_ARCHIVE_TIMEOUT_MS,
     idleNetworkInterval: 100,
